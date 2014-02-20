b6c542c00542c53994479cdb4c6c82d2b3996319
Christopher Petersen <chris@petersen.io>
2014-02-20 07:22:37 -0800

    Initial version of specs

--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -1,4 +1,64 @@
 require 'spec_helper'
+
 describe Basic::Stats do
-  pending "write it"
+  let(:array) { [1,2,3,4,5,6,7,8,9,10].extend Basic::Stats }
+  let(:array_with_outlier) { [1,2,3,4,5,6,7,8,9,30].extend Basic::Stats }
+
+  it "should have a mean of 5.5" do
+    array.mean.should == 5.5
+  end
+
+  it "should have a standard deviation of 3.02765..." do
+    array.standard_deviation.should == 3.0276503540974917
+  end
+
+  it "should have a sample variance of 9.1666..." do
+    array.sample_variance.should == 9.166666666666666
+  end
+
+  it "should have a critical z of 2.29" do
+    array.critical_z.should == 2.29
+  end
+
+  it "should reject an outlier" do
+    expect {
+      array_with_outlier.reject_outliers!
+    }.to change{array_with_outlier.size}.by(-1)
+  end
+
+  describe "z" do
+    it "of 3 should be 0.82572" do
+      array.z(3).should == 0.8257228238447705
+    end
+
+    it "of 10 should be 1.48630" do
+      array.z(10).should == 1.4863010829205867
+    end
+  end
+
+  describe "critical z" do
+    it "of 0 should be nil" do
+      Basic::Stats.critical_z(0).should be_nil
+    end
+
+    it "of 1 should be nil" do
+      Basic::Stats.critical_z(1).should be_nil
+    end
+
+    it "of 3 should be 1.15" do
+      Basic::Stats.critical_z(3).should == 1.15
+    end
+
+    it "of 20 should be 2.71" do
+      Basic::Stats.critical_z(20).should == 2.71
+    end
+
+    it "of 45 should be 3.04" do
+      Basic::Stats.critical_z(45).should == 3.04
+    end
+
+    it "of 200 should be 3.49" do
+      Basic::Stats.critical_z(200).should == 3.49
+    end
+  end
 end
